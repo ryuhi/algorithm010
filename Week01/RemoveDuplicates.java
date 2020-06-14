98f2e5a282d164aa643f1dc2eb54b4cdcaed2d61
张晖 <tobutatu@gmail.com>
2020-06-15 00:56:14 +0800

    添加两道题目的解答

--- Week01/RemoveDuplicates.java
+++ Week01/RemoveDuplicates.java
@@ -6,7 +6,6 @@ public class RemoveDuplicates {
         if ((0 == size) || (1 == size)) {
             return size;
         }
-        if(nums == null || nums.length == 0) return 0;
         int p = 0;
         int q = 1;
         while(q < nums.length){
