0b24b974c1b7313ca6ccbc085c88634dde73e2e0
张晖 <tobutatu@gmail.com>
2020-06-15 00:55:47 +0800

    添加两道题目的解答

---
+++ Week01/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicates {
+
+
+    public int removeDuplicates(int[] nums) {
+        int size = nums.length;
+        if ((0 == size) || (1 == size)) {
+            return size;
+        }
+        if(nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 1;
+        while(q < nums.length){
+            if(nums[p] != nums[q]){
+                if(q - p > 1){
+                    nums[p + 1] = nums[q];
+                }
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+    }
+}
